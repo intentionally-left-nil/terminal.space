62eeffa1a4fd8479be2f7924828e5e4a8c5bf8b2
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-03-30 01:51:02 -0700

    Add projects

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import Terminal from '@anilredshift/react-bash';
-import Resume from './components/resume/Resume';
+import resumeData from './components/resume/resumeData';
 import resumeMarkdown from './components/resume/resumeMarkdown';
+
+const createUrlHandler = url => ((state) => {
+  window.open(url, '_blank');
+  return Object.assign({}, state, {
+    history: state.history.concat([{ value: `Launching ${url}` }]),
+  });
+});
 
 const history = [
   { cwd: '/', value: 'ls' },
-  { value: 'resume' },
+  { value: 'contact projects Resume.md' },
 ];
 
+const mailto = `mailto:${encodeURIComponent(resumeData.contact.email)}?subject=${encodeURIComponent('Contact request from homepage')}`;
+
 const structure = {
-  resume: {
-    'Resume.md': { content: resumeMarkdown },
+  contact: { exec: createUrlHandler(mailto) },
+  'Resume.md': { content: resumeMarkdown },
+  projects: {
+    cogrammers: { exec: createUrlHandler('https://cogrammers.community') },
+    'cogrammers-github': { exec: createUrlHandler('https://github.com/cogrammers/') },
+    'evil-webserver': { exec: createUrlHandler('https://github.com/AnilRedshift/evil-webserver') },
+    'terminal.space': { exec: createUrlHandler('https://github.com/AnilRedshift/terminal.space') },
+    yatlab: { exec: createUrlHandler('https://github.com/anilredshift/yatlab#yatlab') },
   },
 };
 
@@ -23,13 +38,21 @@ class App extends Component {
           history={history}
           structure={structure}
           styles={{
+            ReactBash: {
+              backgroundColor: '#000',
+              padding: '20px',
+            },
             header: {
               display: 'none',
             },
             body: {
               height: '100vh',
-              color: '#FFF',
+              color: '#33FF00',
               backgroundColor: '#000',
+            },
+            prefix: {
+              color: '#33FF00',
+              marginRight: '5px',
             },
           }}
           theme="dark"
