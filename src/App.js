1c776cb85e8bb1d79289a87b8062936f4d70d405
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-03-29 18:03:30 -0700

    Add a terminal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import Terminal from 'react-bash';
 import Resume from './components/resume/Resume';
+
+const history = [
+  { cwd: '/', value: 'ls' },
+  { value: 'resume' },
+];
+
+const structure = {
+  resume: {
+    'Resume.md': { content: 'resume goes here' },
+  },
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Resume />
+      <div>
+        <Terminal
+          prefix="terminal.space"
+          history={history}
+          structure={structure}
+          styles={{
+            header: {
+              display: 'none',
+            },
+            ReactBash: {
+              height: '100vh',
+              backgroundColor: 'black',
+            },
+          }}
+          theme="dark"
+        />
       </div>
     );
   }
