83247c7dcda49467f7fb0e8343df6a3e42538d21
Anil Kulkarni <anil.kulkarni@outlook.com>
2020-05-23 19:17:26 -0700

    Remove stories and add missing peer dependencies

--- src/stories/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-
-import Header from '../components/resume/Header';
-
-storiesOf(Header, module)
-  .add('normal', () => (<Header>Hello World</Header>))
-  .add('right aligned', () => (<Header align="right">Right aligned</Header>));
